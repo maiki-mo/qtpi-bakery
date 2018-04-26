ddbe9b0bdef6c485febff321466367db9d9c9430
maikimo <mike.p.monahan@gmail.com>
2018-04-26 16:16:24 -0400

    added shake up

--- public/main.js
+++ public/main.js
@@ -1,19 +1,22 @@
-// let cupcakeOne = document.querySelector("#first-cake");
-// let cupcakeTwo = document.querySelector("#second-cake");
-// let cupcakeThree = document.querySelector("#third-cake");
+let scramble = document.querySelector("#scramble");
+let cupcakes = document.querySelectorAll(".cupcake-img");
 
-// cupcakeOne.addEventListener("click", wildOutOne);
-// cupcakeOne.addEventListener("click", wildOutTwo);
-// cupcakeOne.addEventListener("click", wildOutThree);
+scramble.addEventListener("onload", wildOut);
 
-// function wildOut() {
-//   cupcakeOne.className = "cupcake-img-spin";
-// }
+function wildOut() {
+  let cupcakes = document.querySelectorAll(".cupcake-img");
+  for (var i in cupcakes) {
+    if (cupcakes.hasOwnProperty(i)) {
+      cupcakes[i].className = 'cupcake-img-spin';
+     } else {
+      cupcakes[i].className = 'cupcake-img';
+     }
+  }
+}
 
-// function wildOutTwo() {
-//   cupcakeTwo.className = "cupcake-img-spin";
-// }
-
-// function wildOutThree() {
-//   cupcakeThree.className = "cupcake-img-spin";
+// var elements = document.getElementsByClassName('hidden-class');
+// for (var i in elements) {
+//   if (elements.hasOwnProperty(i)) {
+//     elements[i].className = 'show-class';
+//   }
 // }
